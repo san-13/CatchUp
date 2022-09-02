125e4ff6222c7b8744054419010bcc88afadd2d5
Sandeep <sandeepvermaslg77@gmail.com>
2022-09-02 17:56:41 +0530

    chore-add initial app directory

---
+++ CatchUp/app/src/main/java/com/sv/catchup/screens/login_signup/loginandsignup.kt
@@ -0,0 +1,10 @@
+package com.sv.catchup.screens.login_signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginandsignup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
